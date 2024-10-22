ee6ac91de182fbf713f1fb0bd1f2698c67aa5b79
natalia001365 <natali.fedunik@gmail.com>
2024-10-22 10:25:32 +0300

    n

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
